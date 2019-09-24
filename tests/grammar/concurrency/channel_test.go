57ae661d629ddb8c200662fd6f6a9c483c089432
chandlersong <chandler605@gmail.com>
2019-09-24 22:59:31 +0800

    keyword: add channel example

---
+++ tests/grammar/concurrency/channel_test.go
@@ -0,0 +1,49 @@
+package concurrency
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+func player(name string, court chan int, wg *sync.WaitGroup, t *testing.T) {
+
+	defer wg.Done()
+
+	for {
+		ball, ok := <-court
+
+		if !ok {
+			t.Logf("Player %s Won \n", name)
+			return
+		}
+
+		n := rand.Intn(100)
+
+		if n%13 == 0 {
+			t.Logf("Player %s Missed\n", name)
+			close(court)
+			return
+		}
+
+		t.Logf("Player %s Hit %d", name, ball)
+		ball++
+		court <- ball
+	}
+}
+
+func TestPlayTennis(t *testing.T) {
+	var wg sync.WaitGroup
+	rand.Seed(time.Now().UnixNano())
+	court := make(chan int)
+
+	wg.Add(2)
+
+	go player("chandler", court, &wg, t)
+	go player("monica", court, &wg, t)
+
+	court <- 1
+
+	wg.Wait()
+}
